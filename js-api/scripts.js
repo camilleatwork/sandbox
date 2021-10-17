5fefa3e7ff1246ccfc9372499f84b0cfaf6d4efe
Camille Tomlin <camille.tomlin@gmail.com>
2021-10-16 21:05:03 -0400

    Removed axois library

--- js-api/scripts.js
+++ js-api/scripts.js
@@ -1,7 +1,6 @@
 const app = document.getElementById('root');
-
 const logo = document.createElement('img');
-logo.src = 'logo.png';
+logo.src = 'sandbox-example-logo.jpg';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
@@ -9,34 +8,34 @@ container.setAttribute('class', 'container');
 app.appendChild(logo);
 app.appendChild(container);
 
-axios.get('https://api.tvmaze.com/shows/912/episodes')
-    .then(response => {
-        response.data.forEach((tv) => {
-            // Create a div with a card class
+var request = new XMLHttpRequest();
+
+request.open('GET', 'http://api.tvmaze.com/shows/912/episodes', true);
+
+request.onload = function () {
+    var data = JSON.parse(this.response);
+    
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(tvshow => {
             const card = document.createElement('div');
             card.setAttribute('class', 'card');
 
-            // Create an h1 and set the text content to the film's title
             const h1 = document.createElement('h1');
-            h1.textContent = tv.name;
+            h1.textContent = tvshow.name;
 
-            // Create a p and set the text content to the film's description
             const p = document.createElement('p');
-            //TO DO: OPTIONAL - Add the episode image [tv.image.medium]
-            tv.summary = tv.summary.substring(0, 300); // Limit to 300 chars
-            p.textContent = `${tv.summary}...`; // End with an ellipses
+            tvshow.summary = tvshow.summary.substring(0, 300);
+            tvshow.summary = tvshow.summary.replace('<p>', '');
+            tvshow.summary = tvshow.summary.replace('</p>', '');
+            p.textContent = `${tvshow.summary}...`;
 
-            // Append the cards to the container element
             container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(p);
+        });
+    } else {
+        console.log('error');
+    }
+}
 
-            // Each card will contain an h1 and a p
-            card.appendChild(h1)
-            card.appendChild(p)
-        })
-    })
-    .catch(function (error) {
-        // handle error
-        const errorMessage = document.createElement('marquee')
-        errorMessage.textContent = `Gah, it's not working!`
-        app.appendChild(errorMessage)
-    })
+request.send();
